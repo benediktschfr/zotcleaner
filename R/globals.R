5cfcd7b8b7306489878a37c325f8ca6bbedfaf1f
Benedikt Claus <benediktclaus@msn.com>
2026-06-07 21:53:40 +0200

    Include globals

---
+++ R/globals.R
@@ -0,0 +1,55 @@
+utils::globalVariables(unique(c(
+  # zot_clean_titles:
+  "clean_value",
+  "fieldID",
+  "fieldName",
+  "needs_casing",
+  "value",
+  "valueID",
+  # zot_find_authors:
+  "firstName",
+  "lastName",
+  # zot_find_institutions:
+  "fieldMode",
+  "firstName",
+  "lastName",
+  # zot_find_journals:
+  "fieldID",
+  "fieldName",
+  "journal",
+  "value",
+  "valueID",
+  # zot_find_publishers:
+  "fieldID",
+  "fieldName",
+  "publisher",
+  "value",
+  "valueID",
+  # zot_find_swapped_names:
+  "fieldMode",
+  "firstName",
+  # zot_fix_institutions:
+  "creatorID",
+  "firstName",
+  "lastName",
+  # zot_get_flat_view:
+  "author_name",
+  "fieldName",
+  "firstName",
+  "itemID",
+  "lastName",
+  "orderIndex",
+  "value",
+  # zot_merge_authors:
+  "creatorID",
+  # zot_merge_journals:
+  "fieldID",
+  "fieldName",
+  "valueID",
+  # zot_merge_publishers:
+  "fieldID",
+  "fieldName",
+  "valueID",
+  # zot_swap_names:
+  "creatorID"
+)))
